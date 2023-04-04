9904b378c4b794d6857f4d861d729bdfa8aceb0c
chuy-zip <andreschuy03@gmail.com>
2023-04-03 19:57:41 -0600

    Added method in reader for Java Priority queue

--- HDT8/src/Reader.java
+++ HDT8/src/Reader.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.PriorityQueue;
 
 public class Reader {
 	
@@ -31,5 +32,35 @@ public class Reader {
 	    
 	    System.out.println("\n");
 	    return heapTree;
+	}
+	
+	public PriorityQueue readTxtFileToPriorityQueueJava() {
+	    String MyFile = "src/pacientes.txt";
+	    System.out.println("Pacientes ingresados");
+	    PriorityQueue<Patient> heapQueue = new PriorityQueue<Patient>();
+	    
+	    try {
+	        BufferedReader reader = new BufferedReader(new FileReader(MyFile));
+	        String line;
+
+	        while ((line = reader.readLine()) != null) {
+	            String[] Strline = line.split(",");
+	            Strline[0] = Strline[0].trim();
+	            Strline[1] = Strline[1].trim();
+	            Strline[2] = Strline[2].trim();
+	            
+	            Patient patient = new Patient(Strline[0], Strline[1], Strline[2]);
+	            heapQueue.add(patient);
+	            System.out.println(patient.getName() + " " + patient.getCondition() + " " + patient.getPriority());
+	        }
+
+	        reader.close();
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    
+	    System.out.println("\n");
+	    return heapQueue;
 	}
 }
--- HDT8/src/main.java
+++ HDT8/src/main.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class main {
@@ -8,14 +9,14 @@ public class main {
 		
 		Scanner scan = new Scanner(System.in);
 		String option = "";
-		
-		
+
 		
 		while(!option.equals("1") && !option.equals("2")) {
 			System.out.println("Bienvenido, cual de las 2 implementaciones desea utilizar:\n"
 					 + "|1| Utilizar la implementacion propia\n"
 					 + "|2| Utilizar la implementacion de Java\n");
 			option = scan.nextLine();
+			BTHeap heap = reader.readTxtFileToPriorityQueue();
 		}
 		
 		if(option.equals("1")) {
@@ -32,9 +33,11 @@ public class main {
 				
 				if(option.equals("1")) {
 					heap.printTree();
+					System.out.println("\n");
 				}
 				else if(option.equals("2")) {
 					heap.remove();
+					System.out.println("\n");
 				}
 				else if(option.equals("3")) {
 					
@@ -50,6 +53,7 @@ public class main {
 					Patient pat = new Patient(name, condition, prio);
 					
 					heap.insert(pat);
+					System.out.println("Paciente ingresado\n");
 				}
 			}
 			
@@ -57,13 +61,38 @@ public class main {
 		
 		else if(option.equals("2")) {
 			
+			PriorityQueue<Patient> PatientQueue = reader.readTxtFileToPriorityQueueJava();
+			
 			while(!option.equals("4")) {
+				
 				System.out.println("Indique la operacion que desea realizar:\n"
 						+ "|1| Ver la lista de pacientes\n"
 						+ "|2| Atender paciente con mayor prioridad\n"
 						+ "|3| Ingresar paciente\n"
 						+ "|4| Salir del programa\n");
 				option = scan.nextLine();
+			}
+			
+			if(option.equals("1")) {
+				
+			}
+			else if(option.equals("2")) {
+				
+			}
+			else if(option.equals("3")) {
+				System.out.println("Ingrese el nombre");
+				String name = scan.nextLine();
+				
+				System.out.println("Ingrese la condicion medica del paciente");
+				String condition = scan.nextLine();
+				
+				System.out.println("Ingrese la prioridad");
+				String prio = scan.nextLine();
+				
+				Patient pat = new Patient(name, condition, prio);
+				
+				PatientQueue.add(pat);
+				System.out.println("Paciente ingresado\n");
 			}
 			
 		}
